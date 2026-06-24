f3969ba9cdcf776cbb15cbf3d0766493db9bd3d7
Frank Winner <115381402+Frank-Winner@users.noreply.github.com>
2026-06-24 23:50:00 +0100

    Add files via upload

---
+++ 403.php
@@ -0,0 +1,28 @@
+<?php require_once 'includes/header.php'; ?>
+
+<div class="container py-5 text-center">
+
+    <h1 class="display-1">
+        403
+    </h1>
+
+    <h3>
+        Access Forbidden
+    </h3>
+
+    <p>
+        You do not have permission
+        to access this resource.
+    </p>
+
+    <a
+        href="index.php"
+        class="btn btn-primary">
+
+        Go Home
+
+    </a>
+
+</div>
+
+<?php require_once 'includes/footer.php'; ?>
